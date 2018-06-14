c4faf3e73417925322930750e7599aeb6d49af41
julianGonzalezV <j.andres2087@gmail.com>
2018-06-13 22:41:48 -0500

    basic setup

--- 04-spotiapp/src/app/app.component.ts
+++ 04-spotiapp/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+/*
+  Playing with spotify
+
+   ID 456ede39a2ab47569c11a545a0fa5fdb
+   f9663f79aa4a4d97a01a427357f073e5 */
+
   title = 'app';
 }
