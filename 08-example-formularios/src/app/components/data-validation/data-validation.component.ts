16e3ebb07b19475f54d8c2b2b729b944d23273ac
julianGonzalezV <j.andres2087@gmail.com>
2018-06-20 22:39:26 -0500

    End of forms classes

--- 08-example-formularios/src/app/components/data-validation/data-validation.component.ts
+++ 08-example-formularios/src/app/components/data-validation/data-validation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-data-validation',
@@ -16,7 +17,7 @@ export class DataValidationComponent implements OnInit {
       apellido: 'gonzalez'
     } ,
     correo: 'j.andres2087@gmail.com'
-    //pasatiempo: ["Netflix","Programming","gym"]
+    // pasatiempo: ["Netflix","Programming","gym"]
   }
 
   constructor(  ) { 
@@ -35,15 +36,27 @@ export class DataValidationComponent implements OnInit {
        'pasatiempo': new FormArray([
          new FormControl('gym', Validators.required)
        ]),
+       'username': new FormControl('', [Validators.required], this.userExist),
        'password1': new FormControl('', [Validators.required]),
        'password2': new FormControl()
     });
 
-    //this.noIgual.bind( this.formaDesdeData )
-    //bind es una funcion Js que lo que dice acá es que al llamar la funcion noIgual y dentro de esta
-    //se escriba this  ..ese this va a ser el formaDesdeData, estableciendo un contexto 
+    // this.noIgual.bind( this.formaDesdeData )
+    // bind es una funcion Js que lo que dice acá es que al llamar la funcion noIgual y dentro de esta
+    // se escriba this  ..ese this va a ser el formaDesdeData, estableciendo un contexto 
     this.formaDesdeData.controls['password2'].setValidators([Validators.required, this.noIgual.bind( this.formaDesdeData )]);
-    //this.formaDesdeData.setValue(this.usuario);
+    // this.formaDesdeData.setValue(this.usuario);
+
+    // De esta manera escucha los cambios de todo el formulario, es decir que cada campo que este contenga
+    /*this.formaDesdeData.valueChanges
+                       .subscribe( data => console.log('data from valueChanges', data) );*/
+
+    // De esta manera solo escucha los cambios de un solo atributo del formulari o
+    this.formaDesdeData.controls['username'].valueChanges
+                       .subscribe( data => console.log('data from valueChanges', data) );
+
+    this.formaDesdeData.controls['username'].statusChanges
+                       .subscribe( data => console.log('data from statusChanges', data) );
   }
 
   //si el formulario es muy grande entonces podría colocar la logica del formulario acá
@@ -88,17 +101,37 @@ export class DataValidationComponent implements OnInit {
 
 
 
-  noIgual( control: FormControl ): {[s:string]: boolean} {
-    //como se le hizo bind arriba, ver linea 45 ahora this =  this.formaDesdeData
+  noIgual( control: FormControl ): {[s: string]: boolean} {
+    // como se le hizo bind arriba, ver linea 45 ahora this =  this.formaDesdeData
     let forma = this;
-    if ( control.value !== forma.controls['password1'].value ){
+    if ( control.value !== forma.controls['password1'].value ) {
       return {
         passwordnotequal: true
-      }
+      };
     }
 
-    //sí regresa null quiere decir que todo está bien
+    // sí regresa null quiere decir que todo está bien
     return null;
+  }
+
+
+
+   // Lo ideal es que se tenga un archivo de VALIDACIONES PERSONALIZADAS ASINCRONAS, por ahora vamos a verlo así:
+
+   userExist( control: FormControl ): Promise<any> | Observable<any> {
+    let promesa = new Promise(
+      ( resolve, reject ) => {
+        setTimeout(() => {
+          if ( control.value === 'juliancho' ) {
+            resolve({ exists: true} );
+          } else {
+            resolve(null);
+          }
+        }, 3000);
+
+      }
+    );
+    return promesa;
   }
 
 }
