66df826fbc4992ab1dcfcc406b951e9d5ae5cc24
julianGonzalezV <j.andres2087@gmail.com>
2018-06-27 08:04:37 -0500

    set css for only one component

--- 06-miscelaneous/src/app/components/css-for-one/css-for-one.component.ts
+++ 06-miscelaneous/src/app/components/css-for-one/css-for-one.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+//al hacer el style para los parrafos se va a notar que solo lo aplica para este 
+//y no para todos los parrafos, pues angula rle asigana otra eqtiqueta diferente a lo que pertenece a este
+//compo ..note que al dar inspeccionar elemento existe un tag _ngcontent-c1=""  y los demás(fuera del componente) tienen otro
+//diferentes
 @Component({
   selector: 'app-css-for-one',
   template: `
     <p>
       css-for-one works!
     </p>
+    <p>
+      css-for-one works2!
+    </p>
   `,
-  styles: []
+  styles: [`
+    p {
+      color: red;
+      font-size: 20 px;
+    }
+    `]
 })
 export class CssForOneComponent implements OnInit {
 
