d3ec04c1ad57c24048368b3877210be9eca8b722
julianGonzalezV <j.andres2087@gmail.com>
2018-08-22 13:34:37 -0500

    chat firebase and angularfire2

---
+++ 10-firechat/src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCCCxs08w2GtRYDIk81QiyyjlrtnDVdNiI",
+    authDomain: "firechat-69937.firebaseapp.com",
+    databaseURL: "https://firechat-69937.firebaseio.com",
+    projectId: "firechat-69937",
+    storageBucket: "firechat-69937.appspot.com",
+    messagingSenderId: "760105820334"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
