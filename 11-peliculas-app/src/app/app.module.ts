14ba232894dd84d6411318e797fc5519c9f6768f
julianGonzalezV <j.andres2087@gmail.com>
2018-08-31 16:00:08 -0500

    creando componenetes principales

--- 11-peliculas-app/src/app/app.module.ts
+++ 11-peliculas-app/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { HttpModule, JsonpModule } from '@angular/http';
 //services
 import { PeliculaServiceService } from './services/pelicula-service.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule, 
--- 11-peliculas-app/src/app/services/pelicula-service.service.ts
+++ 11-peliculas-app/src/app/services/pelicula-service.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Http, Jsonp } from '@angular/http';
 import { map } from "rxjs/operators";
+/*
+import "rxjs/Rx";
+import { map, filter, scan } from 'rxjs/operators';
+import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
+
+*/
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculaServiceService {
-  private apiKey: string = '9357df4ee31aeb708c0532a2b98d4cb8';
-  private urlMovieDb: string = 'https://www.themoviedb.org/3';
+  private apiKey: string = "9357df4ee31aeb708c0532a2b98d4cb8";
+  private urlMovieDb: string = "https://api.themoviedb.org/3";
 
   constructor(private jsonp:Jsonp, private http:Http) { }
-
   getPopulares(){
     // Así saldría error de cross domain, descomente paa que pruebe
     // let url = `${ this.urlMovieDb }/discover/movie?sort_by=popularity.desc&api_key= ${this.apiKey}&language=es;`
@@ -17,9 +22,13 @@ export class PeliculaServiceService {
     
     // Solucion a cross domain
     //note el callback al final de la url 
-    let url = `${ this.urlMovieDb }/discover/movie?sort_by=popularity.desc&api_key= ${this.apiKey}&language=es&callback=JSONPCALLBACK;`
-
+    let url = `${ this.urlMovieDb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apiKey }&language=es&callback=JSONP_CALLBACK`;
     return this.jsonp.get( url ).pipe(map( res=> res.json()));
 
+  }
+
+  buscarPelicula(text:string){
+    let url = `${ this.urlMovieDb }/search/movie?query=${ text }&sort_by=popularity.desc&api_key=${ this.apiKey }&language=es&callback=JSONP_CALLBACK`;
+    return this.jsonp.get( url ).pipe(map( res=> res.json()));
   }
 }
