181f05b886cceb24f9fa8eb57dd4b4d26858d209
julianGonzalezV <j.andres2087@gmail.com>
2018-09-15 17:06:49 -0500

    graphs library

--- 15-fotosfirebase/src/app/app.module.ts
+++ 15-fotosfirebase/src/app/app.module.ts
@@ -1,14 +1,23 @@
+import { APP_ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotosComponent,
+    CargaComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
